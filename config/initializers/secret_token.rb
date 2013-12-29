cee2c7b7fc770a14684000b17ee0aa98c99f33a9
matroshka <matroshka.mint@gmail.com>
2013-12-29 01:46:00 +0100

    Depot Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubytom02::Application.config.secret_key_base = '575fe748c6d8163691e991a682c67c8b2ece08c7505fd04c3ae3451e9bd8cf6f2e55acb3d1df0a22f30fefa84e7baa39e1bbcf865a6d8f566b9ed691dc939522'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rubytom02::Application.config.session_store :cookie_store, key: '_rubytom02_session'
